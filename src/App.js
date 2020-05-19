91ad62f695a9646ec3dbac1cf5c05b55c3bafccd
Matheus Santos <matheus.psantos2016@gmail.com>
2020-05-19 19:58:32 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Form from './components/NewForm';
+
+import './global.css';
+
+function src() {
+  return (
+    <div className='App'>
+      <Form/>
+    </div>
+  );
+}
+
+export default src;
